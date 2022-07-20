657118e88cd6d62494230f3bb08afdc42246b690
radioherz2022 <jes1025k@gmail.com>
2022-07-20 18:29:15 +0900

    강의 8일차

---
+++ JS/01_variable.js
@@ -0,0 +1,62 @@
+// 변수 메모리에 공간 확보하고 데이터를 저장한 후 저장된 메모리의 주소.
+// -> 데이터를 저장하고 꺼내 쓸 수 있는 주머니.
+var num = 10;
+
+console.log(num);
+
+// 변수 선언 - 값을 저장할 메모리 공간을 마련하고 메모리에 접근할 수 있는 이름 지정.
+// 변수 선언만 하면 undefined가 초기화.
+var age;
+console.log(age);
+
+// 변수 선언에는 var, let, const(상수) 키워드 사용.
+// 변수 선언 키워드 뒤에 변수명을 지정.
+
+var year; // year라는 이름의 변수 선언.
+let month; // month라는 이름의 변수 선언.
+const DAY = 10; // DAY라는 이름의 상수 선언.
+
+// 호이스팅:JS 선언된 변수 함수를 실행 전에 읽는다. -> var 키워드에만 해당.
+
+console.log(score); // var 키워드 사용 -> 호이스팅 발생 -> 에러 발생하지 않음
+var score;
+//console.log(birthDay); // let 키워드 사용 -> 호이스팅 발생하지 않음. -> 에러 발생
+let birthDay;
+
+// 할당 : 선언된 변수(메모리)에 값을 저장.
+score = 90; // score라는 변수에 90이라는 값을 할당(저장).
+console.log(score);
+score = 100; // score라는 변수에 100이라는 값을 재할당.
+
+// 선언과 할당 동시에 가능 -> 초기화.
+console.log("firstName:", firstName); // 선언부분만 호이스팅.
+var firstName = "seok";
+
+// 참조:변수에 저장된 값을 읽는다.
+console.log(firstName); //firstName 변수의 값을 참조하여 출력
+
+// 변수 이름 특수문자 제외 문자, 숫자, _(언더), $ 사용 가능
+// 변수 이름은 숫자로 시작할 수 없다. ex) var 01num; -> 에러.
+// 변수 이름은 대소문자 구분 => firstname과 FIRSTNAME은 다른 변수이다.
+
+// first name => firstname : 카멜 케이스.
+// first name => first_name : 스네이크 케이스.
+// first name => FirstName : 파스칼 케이스 => 클래스 명에 주로 사용.
+
+// 문제1 num1과 num2를 선언하고 각각 20, 30을 할당하여 출력
+
+var num1, num2;
+num1 = 20;
+num2 = 30;
+console.log("Q1", num1, num2);
+
+// 문제2 num1과 num2를 더한 값을 result 변수에 할당하여 출력
+var result = num1 + num2;
+console.log("Q2", result);
+
+// 문제3 num1과 num2를 서로 바꿔서 할당
+
+var temp = num1; // 20을 저장할 임시 공간 변수
+num1 = num2; // 30을 num1에 할당
+num2 = temp; // temp에 임시 저장한 20을 num2에 할당
+console.log("Q3", num1, num2);
---
+++ JS/02_date_type.js
@@ -0,0 +1,65 @@
+// 식: 값으로 평가돼서 새로운 값을 생성.
+// => 값으로 평가될 수 있는 모든게 식.
+var result = 10 + 30;
+
+// 문:프로그램을 구성하는 최소 단위. 식에 문에 포함
+var x; // 변수 선언문. 값으로 평가가 될 수 없기 때문에 식이 아님.
+if (true) console.log("진실"); //조건문
+
+// 자료형:프로그램이 처리할 수 있는 자료의 형태.
+
+// 숫자형(number):""없이 숫자만 입력.
+var year = 2022; // 정수
+const PI = 3.14; // 실수
+console.log(typeof year, typeof PI); // JS에서는 정수와 실수 구분 없음.
+console.log(0.1 + 0.2);
+
+// 문자열(string) : 문자를 나타내는 데이터. ""(큰따옴표). ''(작은따옴표)안에 문자를 작성.
+// -> JS에서는 ""와 '' 구분 X -> 문자와 문자열 구분 X
+var firstName = "'seok'";
+var strNum = "1"; // 숫자 X 문자열 O
+console.log(1 + "2"); // JS에서는 숫자와 문자를 더해도 에러 X
+console.log(typeof firstName, firstName);
+
+// 논리형(boolean):참과 거짓으로 표현되는 자료형. => true, false
+var isTrue = true;
+console.log(typeof isTrue);
+
+// 조건식의 결과로 반환. => 조건문에 사용.
+var isBig = 10 > 100;
+console.log(isBig);
+
+// undefined : 값이 정의되지 않은 상태를 나타내는 데이터. => 개발자의 의도 반영 X.
+// null : 개발자가 유효하지 않은 상태라고 명시적으로 나타내는 데이터 => 개발자의 의도 반영 O
+var data;
+console.log(data); // 변수 선언 후 값 할당 X => undefined(정의되지 않은) 출력.
+data = null; // 개발자가 의도적으로 data 변수가 유효하지 않다고 명시.
+if (data == null) console.log("실행");
+
+// 배열(array) : 여러개의 데이터를 저장
+// 순서(인덱스)로 데이터에 접근 => 순서는 0부터 시작, 즉 첫번째 0 두번째 1.....
+// 배열[인덱스(숫자)] 형식으로 사용 => 숫자로 평가되는 식 사용 가능
+var arr = [1, 4, 10];
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[10 - 9]);
+
+// 객체(object):여러개의 데이터 저장 가능 => key값으로 데이터 참조.
+// key(속성이름):value(속성값)
+var obj = { age: 30, firstName: "seok", lastName: "hwangbo" };
+console.log("나이는", obj.age);
+
+// user1, user2를 이름,나이,키 => 둘 나이의 합과 둘 키의 차를 출력.
+var user1 = {
+  name: "seok",
+  age: 30,
+  height: 173,
+};
+var user2 = {
+  name: "minsu",
+  age: 34,
+  height: 182,
+};
+
+console.log("나이합은", user1.age + user2.age);
+console.log("키의차이는", user1.height - user2.height);
