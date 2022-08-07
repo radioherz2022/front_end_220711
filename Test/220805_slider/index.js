91c5aad51e422d437d2ab1b0ef1c136fcd9aa24a
radioherz2022 <jes1025k@gmail.com>
2022-08-08 08:59:22 +0900

    Test공부

--- Test/220805_slider/index.js
+++ Test/220805_slider/index.js
@@ -1,11 +1,52 @@
 
 let btnSide = document.querySelector(".menuIcon");
-let gnbList = document.querySelector(".gnbList");
+let gnbList = document.querySelector("#gnbList");
 btnSide.addEventListener("click", function () {
   gnbList.classList.add("on");
 });
-gnbList.addEventListener("click", function (event) {
-  if (event.target === event.currentTarget) {
+
+
+
+window.addEventListener("resize", function () {
+  if (this.innerWidth > 768) {
     gnbList.classList.remove("on");
   }
 });
+
+
+let slideContainer = document.querySelector(".slideContainer");
+let slideWrapper = document.querySelector(".slideContainer ul");
+let slideImg = document.querySelector(".slide")
+let slideBtn = document.querySelector(".btnBox")
+let slideLeft = document.querySelector(".slideLeft")
+let slideRight = document.querySelector(".slideRight")
+
+
+slideRight.addEventListener("click", function () {
+  slideWrapper.style.transform = `translateX(${-100 }vw)`;
+});
+
+slideLeft.addEventListener("click", function () {
+  slideWrapper.style.transform = `translate(${0}vw)`;
+});
+
+
+
+
+// 모달창 클릭해서 보여주기
+
+let modalBg = document.querySelector(".modalBg");
+let modal = modalBg.querySelector(".modal");
+let btnClose = modalBg.querySelector(".btnClose");
+let liConnect = document.querySelector(".listLi")
+
+btnClose.addEventListener("click", function () {
+  modalBg.classList.add("hide");
+});
+
+
+liConnect.addEventListener("click", function(){
+  modal.classList.remove("hide");
+})
+
+
